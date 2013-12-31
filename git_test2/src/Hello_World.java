f927e36672858b7cf68637781f57c3ea485e3382
dha0921 <dha0921@naver.com>
2013-12-31 17:19:11 +0900

    add Print method

--- git_test2/src/Hello_World.java
+++ git_test2/src/Hello_World.java
@@ -2,5 +2,10 @@
 public class Hello_World {
 	public static void main (String[]args){
 		System.out.println("Hello Wrold");
+		Print();
 	}
+	public static void Print(){
+		System.out.println("Using Print Method");
+	}
+
 }
