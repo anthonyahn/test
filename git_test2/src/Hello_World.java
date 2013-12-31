97679bec9225a92f2f6ed4bfe4fc500658d5a098
dha0921 <dha0921@naver.com>
2013-12-31 17:48:29 +0900

    #4 add Print method use "println"
    Signed-off-by: dha0921 <dha0921@naver.com>

--- git_test2/src/Hello_World.java
+++ git_test2/src/Hello_World.java
@@ -1,10 +1,11 @@
 public class Hello_World {
 	public static void main (String[]args){
-		System.out.println("Hello Wrold");
+		System.out.println("Hello World");
 		Print();
 	}
 	public static void Print(){
 		System.out.println("Using Print Method");
+		System.out.println("Hello World");
 	}
-
+	
 }
