6fad31e06e103205cc6bb9a04aeaa15c81845516
dha0921 <dha0921@naver.com>
2013-12-31 18:08:16 +0900

    #7 create Hello_Korea

---
+++ Hello Korea/src/Hello_Korea.java
@@ -0,0 +1,12 @@
+
+public class Hello_Korea {
+	public static void main (String[]args){
+		System.out.println("Hello Korea");
+		Print();
+	}
+	public static void Print(){
+		System.out.println("Using Print Method");
+		System.out.println("Hello Korea");
+	}
+	
+}
